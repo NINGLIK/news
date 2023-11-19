2f3af9dd55871d791ae4c50beedbe4bbc2d5f47e
NINGLIK <2573115865@qq.com>
2023-11-19 12:21:55 +0800

    Commit on 2023/11/19 -     rhythm is just a *click* away!

--- DoingIt/git-push.sh
+++ DoingIt/git-push.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 # 获取当前日期和时间
-currentDate=$(date "+%Y-%m-%d")
+currentDate=$(date "+%Y/%m/%d")
 
 # 删除日期中的可能存在的空格
 currentDate=$(echo "$currentDate" | tr -d ' ')
 
 # 在时间后面添加其他内容
-title="$1"
-commit_message="Commit on $currentDate - $title
-rhythm is just a *click* away!"
+title="$title"
+content="$content"
+
+if [ -z "$content" ]; then
+    commit_message="Commit on $currentDate - $title
+    rhythm is just a *click* away!"
+else
+    commit_message="Commit on $currentDate - $title
+    $content"
+fi
 
 # 用ssh连接github
 ssh -T git@github.com
