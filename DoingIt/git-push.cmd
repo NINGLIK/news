533cf8bab5af08282009132f0056b7263ba13d3c
NINGLIK <2573115865@qq.com>
2023-11-19 11:57:06 +0800

    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- DoingIt/git-push.cmd
+++ DoingIt/git-push.cmd
@@ -2,18 +2,18 @@
 
 REM 获取当前日期和时间
 set "current_date=%DATE%"
-set "current_time=%TIME%"
+@REM set "current_time=%TIME%"
 
 REM 删除日期中的可能存在的空格
 set "current_date=%current_date: =%"
 
 REM 格式化时间，以删除不需要的字符
-set "current_time=%current_time::=%"
-set "current_time=%current_time:.=%"
+@REM set "current_time=%current_time::=%"
+@REM set "current_time=%current_time:.=%"
 
 REM 在时间后面添加其他内容
 set "custom_text=%1"
-set "commit_message=Commit on %current_date% at %current_time% - %custom_text%"
+set "commit_message=Commit on %current_date% - %custom_text%"
 
 REM 用ssh连接github
 ssh -T git@github.com
