81d0efa5510ace2e309bcc1af5f803997a56f692
NINGLIK <2573115865@qq.com>
2023-11-19 12:08:40 +0800

    Commit on 2023-11-19 - HI,osu! rhythm is just a *click* away!

--- DoingIt/git-push.cmd
+++ DoingIt/git-push.cmd
@@ -1,28 +1,30 @@
 @echo off
 
-REM 获取当前日期和时间
-set "current_date=%DATE%"
+@REM 获取当前日期和时间
 @REM set "current_time=%TIME%"
+set "currentDate=%DATE%"
 
-REM 删除日期中的可能存在的空格
-set "current_date=%current_date: =%"
+@REM 删除日期中的可能存在的空格
+set "currentDate=%currentDate: =%"
+set "currentDate=%currentDate:~0,10%"
 
-REM 格式化时间，以删除不需要的字符
+@REM 格式化时间，以删除不需要的字符
 @REM set "current_time=%current_time::=%"
 @REM set "current_time=%current_time:.=%"
 
-REM 在时间后面添加其他内容
-set "custom_text=%1"
-set "commit_message=Commit on %current_date% - %custom_text%"
+@REM 在时间后面添加其他内容
+set "title=%1"
+set "commit_message=Commit on %currentDate% - %title%^
+rhythm is just a *click* away!"
 
-REM 用ssh连接github
+@REM 用ssh连接github
 ssh -T git@github.com
 
-REM 添加所有更改到暂存区
+@REM 添加所有更改到暂存区
 git add .
 
-REM 提交更改，并添加提交消息
+@REM 提交更改，并添加提交消息
 git commit -m "%commit_message%"
 
-REM 推送到远程仓库（如果需要的话）
+@REM 推送到远程仓库（如果需要的话）
 git push origin master:master
--- DoingIt/git-push.sh
+++ DoingIt/git-push.sh
@@ -1,8 +1,24 @@
-#用ssh连接github
+#!/bin/bash
+
+# 获取当前日期和时间
+currentDate=$(date "+%Y-%m-%d")
+
+# 删除日期中的可能存在的空格
+currentDate=$(echo "$currentDate" | tr -d ' ')
+
+# 在时间后面添加其他内容
+title="$1"
+commit_message="Commit on $currentDate - $title
+rhythm is just a *click* away!"
+
+# 用ssh连接github
 ssh -T git@github.com
+
 # 添加所有更改到暂存区
 git add .
+
 # 提交更改，并添加提交消息
-git commit -m "$1"
+git commit -m "$commit_message"
+
 # 推送到远程仓库（如果需要的话）
 git push origin master:master
